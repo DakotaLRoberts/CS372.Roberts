dab0ea25306d650812d06bbd031c11d83fd3426d
DakotaLRoberts <128983616+DakotaLRoberts@users.noreply.github.com>
2023-08-21 15:03:51 -0500

    Update asg1part1.cpp

--- apps/asg1/asg1part1.cpp
+++ apps/asg1/asg1part1.cpp
@@ -6,61 +6,64 @@ string* part3one();
 void printpart3();
 int main() {
 	//part1();
-	part2();
-	//printpart3();
+	//part2();
+	printpart3();
 	return 0;
 }
 void part1() {
-	
+
 
 	int const size = 15;
 	int* arr = new int[size];
 	for (int i = 0; i < size; i++) {
-	
+
 		arr[i] = i;
 		cout << arr[i] << endl;// using this line to show what was put into the array 
 	}
 
 
 	int* secondpoint = arr;
-	delete [] arr;
+	delete[] arr;
 	for (int i = 0; i < 10; i++) {
 		cout << secondpoint[i] << " ";
 	}
 
 
 }
-void part2(){
+void part2() {
 	int const size = 15;
-	
+
 	int* firstpoint = new int[size];
-	int* secondpoint = firstpoint;
+
 	for (int i = 0; i < size; i++) {
-		
+
 		firstpoint[i] = i;
 		cout << firstpoint[i] << endl;// using this line to show what was put into the array 
 	}
-
+	int* secondpoint = firstpoint;
 	delete firstpoint;
 
-	cout << "This is the address of the second pointer" &secondpoint << endl;
-	
+	cout << &secondpoint << endl;
+
 	for (int i = 0; i < 10; i++) {
 		cout << secondpoint[i] << " ";
 	}
-	
 
-	
+
+
 
 
 }
-string* part3one() {
+void part3one(string& random) {
 	string* str = new string("abcdefghijkl");
-	return str;
+	random = *str;
+	delete str;
 }
 
 void printpart3() {
-	string* str = part3one();
-	cout << *str << endl;
-	delete str; 
+	string finalstring;
+	part3one(finalstring);
+
+	cout << finalstring << endl;
+
 }
